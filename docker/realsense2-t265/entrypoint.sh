56f78317a5781168d959caf04ed887ca06bc2738
Igor <igor@cogniteam.com>
2020-09-17 15:32:39 +0300

    realsense t265 docker added

---
+++ docker/realsense2-t265/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+# setup ros environment
+. /opt/ros/$ROS_DISTRO/setup.bash
+
+CAMERA_NAME="${1:-camera}"
+SERIAL_NO="${2}"
+
+X=${3:-0}
+Y=${4:-0}
+Z=${5:-0}
+YAW=${6:-0}
+PITCH=${7:-0}
+ROLL=${8:-0}
+
+rosrun tf2_ros static_transform_publisher ${X} ${Y} ${Z} ${YAW} ${PITCH} ${ROLL} base_link ${CAMERA_NAME}_link &
+
+roslaunch realsense2_camera rs_t265.launch enable_pointcloud:=true camera:=${CAMERA_NAME} serial_no:=${SERIAL_NO}
