1389018d56df960fdbe96e701a49fe25fcda3525
ari <ari@cogniteam.com>
2020-09-24 15:46:36 +0300

    optional update D435

--- docker/realsense2-updater/entrypoint.sh
+++ docker/realsense2-updater/entrypoint.sh
@@ -4,3 +4,12 @@ set -e
 COMMAND="${1:-"-l"}"
 
 rs-fw-update $COMMAND
+
+read -p "Do you want to update a single connected D435 camera ? (Y/n) " answer
+case ${answer} in
+n|N)
+    ;;
+*)
+    rs-fw-update -f Signed_Image_UVC_5_12_7_100.bin
+    ;;
+esac
