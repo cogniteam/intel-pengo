e362165b7dd7008ddfa2c32a6bb1aee88bcf2c33
Daria <daria@cogniteam.com>
2020-09-30 17:47:50 +0300

    sleep after docker run added(demo script)

--- launch_demo_tracking.bash
+++ launch_demo_tracking.bash
@@ -10,14 +10,15 @@ sleep 20
 #
 # OpenVino Myriad
 #
-docker run -e ROS_MASTER_URI -e ROS_IP --net=host --privileged --rm -v /dev:/dev intelpengo/openvino bash -ic "roslaunch vino_launch pengo_detection.launch myriad:=true camera_name:=cam1"
+docker run -e ROS_MASTER_URI -e ROS_IP --net=host --privileged --rm -v /dev:/dev intelpengo/openvino bash -ic "roslaunch vino_launch pengo_detection.launch myriad:=true camera_name:=cam1" &
 sleep ${SLEEP_TIME}
 
 #
 # D435 front camera
 #
-docker run -e ROS_MASTER_URI -e ROS_IP --privileged --net=host --rm intelpengo/realsense2 /rs_camera.sh cam1 939622072996 0.17 0 0.27 0 0 0
+docker run -e ROS_MASTER_URI -e ROS_IP --privileged --net=host --rm intelpengo/realsense2 /rs_camera.sh cam1 939622072996 0.17 0 0.27 0 0 0 &
 # sleep ${SLEEP_TIME}
+sleep 10
 
 #
 # T265
@@ -27,24 +28,28 @@ docker run -e ROS_MASTER_URI -e ROS_IP --privileged --net=host --rm intelpengo/r
 #
 # Kobuki
 #
-docker run -e ROS_MASTER_URI -e ROS_IP --net=host --privileged --rm intelpengo/kobuki-driver:latest /dev/ttyUSB1
+docker run -e ROS_MASTER_URI -e ROS_IP --net=host --privileged --rm intelpengo/kobuki-driver:latest /dev/ttyUSB1 &
+sleep 1
 
-docker run -e ROS_MASTER_URI -e ROS_IP --net=host intelpengo/kobuki-navigation:latest
+docker run -e ROS_MASTER_URI -e ROS_IP --net=host intelpengo/kobuki-navigation:latest &
+sleep 1
 
 #
 # Person follower
 #
 
-docker run -e ROS_MASTER_URI -e ROS_IP --net=host intelpengo/person-follower:latest
+docker run -e ROS_MASTER_URI -e ROS_IP --net=host intelpengo/person-follower:latest &
+sleep1
 
 #
 # ROS slam-toolbox
 #
 
-docker run -e ROS_MASTER_URI -e ROS_IP --net=host intelpengo/slam-toolbox:latest
+docker run -e ROS_MASTER_URI -e ROS_IP --net=host intelpengo/slam-toolbox:latest &
+sleep1
 
 #
 # RP Lidar
 #
 
-docker run -e ROS_MASTER_URI -e ROS_IP --rm -it --net=host --privileged intelpengo/rplidar 0 0 0.22 -1.5707 0 3.1415
+docker run -e ROS_MASTER_URI -e ROS_IP --rm -it --net=host --privileged intelpengo/rplidar 0 0 0.22 -1.5707 0 3.1415 &
