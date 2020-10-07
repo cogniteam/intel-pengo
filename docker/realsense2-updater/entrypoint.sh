f8b741b68217df6f7930706cfb948ad671afca28
ari <yakirari@gmail.com>
2020-10-07 09:56:01 +0300

    realsense-updater argument fix

--- docker/realsense2-updater/entrypoint.sh
+++ docker/realsense2-updater/entrypoint.sh
@@ -3,13 +3,15 @@ set -e
 
 COMMAND="${1:-"-l"}"
 
-rs-fw-update $COMMAND
-
-read -p "Do you want to update a single connected D435 camera ? (Y/n) " answer
-case ${answer} in
-n|N)
-    ;;
-*)
-    rs-fw-update -f Signed_Image_UVC_5_12_7_100.bin
-    ;;
-esac
+if [ "$COMMAND" = 'update' ]; then
+    read -p "Do you want to update a single connected D435 camera ? (Y/n) " answer
+    case ${answer} in
+    n|N)
+        ;;
+    *)
+        rs-fw-update -f Signed_Image_UVC_5_12_7_100.bin
+        ;;
+    esac
+else
+    rs-fw-update $COMMAND
+fi 
