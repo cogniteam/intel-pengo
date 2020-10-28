21477978d419f8532162ca7968a4b25227753a03
Igor <igor@cogniteam.com>
2020-10-28 15:14:02 +0200

    t265 odometry translation

--- docker/realsense2-t265/rs_camera.sh
+++ docker/realsense2-t265/rs_camera.sh
@@ -3,6 +3,7 @@ set -e
 
 # setup ros environment
 . /opt/ros/$ROS_DISTRO/setup.bash
+. /catkin_ws/devel/setup.bash
 
 CAMERA_NAME="${1:-camera}"
 SERIAL_NO="${2}"
@@ -15,15 +16,8 @@ YAW=${6:-0}
 PITCH=${7:-0}
 ROLL=${8:-0}
 
-X_INVERSE=$(bc <<< "-${3:-0}")
-Y_INVERSE=$(bc <<< "-${4:-0}")
-Z_INVERSE=$(bc <<< "-${5:-0}")
-YAW_INVERSE=$(bc <<< "-${6:-0}")
-PITCH_INVERSE=$(bc <<< "-${7:-0}")
-ROLL_INVERSE=$(bc <<< "-${8:-0}")
+# rosrun tf2_ros static_transform_publisher ${X_INVERSE} ${Y_INVERSE} ${Z_INVERSE} ${YAW_INVERSE} ${PITCH_INVERSE} ${ROLL_INVERSE} ${CAMERA_NAME}_pose_frame base_footprint &
+# rosrun tf2_ros static_transform_publisher ${X} ${Y} ${Z} ${YAW} ${PITCH} ${ROLL} odom ${CAMERA_NAME}_odom_frame &
+# rosrun topic_tools relay /${CAMERA_NAME}/odom/sample /odom &
 
-rosrun tf2_ros static_transform_publisher ${X_INVERSE} ${Y_INVERSE} ${Z_INVERSE} ${YAW_INVERSE} ${PITCH_INVERSE} ${ROLL_INVERSE} ${CAMERA_NAME}_pose_frame base_footprint &
-rosrun tf2_ros static_transform_publisher ${X} ${Y} ${Z} ${YAW} ${PITCH} ${ROLL} odom ${CAMERA_NAME}_odom_frame &
-rosrun topic_tools relay /${CAMERA_NAME}/odom/sample /odom &
-
-exec roslaunch realsense2_camera rs_t265_pengo.launch enable_fisheye1:=true enable_fisheye2:=true camera:=${CAMERA_NAME} serial_no:=${SERIAL_NO} publish_odom_tf:=true
+exec roslaunch realsense2_camera rs_t265_pengo.launch enable_fisheye1:=true enable_fisheye2:=true camera:=${CAMERA_NAME} serial_no:=${SERIAL_NO} publish_odom_tf:=true x:=${X} y:=${Y} z:=${Z} roll:=${roll} pitch:=${pitch} yaw:=${yaw}
