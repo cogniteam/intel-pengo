5841b3187b351f216a16d3e83ba8de7453ca4081
ari <yakirari@gmail.com>
2020-12-03 13:37:23 +0200

    cmd_vel topic arg

--- docker/keyboard-teleop/entrypoint.sh
+++ docker/keyboard-teleop/entrypoint.sh
@@ -4,12 +4,7 @@ set -e
 # setup ros environment
 . /opt/ros/$ROS_DISTRO/setup.bash
 
-X=${1:-0}
-Y=${2:-0}
-Z=${3:-0}
-YAW=${4:-0}
-PITCH=${5:-0}
-ROLL=${6:-0}
+CMD_VEL=${1:-cmd_vel}
 
 # Twist from keyboard
-exec rosrun teleop_twist_keyboard teleop_twist_keyboard.py
+exec rosrun teleop_twist_keyboard teleop_twist_keyboard.py cmd_vel:=${CMD_VEL}
